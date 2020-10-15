18bea8cdddaea0327d812d8129b008dd6e0412df
Georgi Ivaylov Georgiev <georgi4c@gmail.com>
2020-10-15 10:40:52 +0300

    Add Startup, Infrastructure and Web projects skeletons

---
+++ Homex.Infrastructure/Common/Persistence/HomexDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Homex.Domain.Common.Models;
+using Homex.Domain.Shelves.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homex.Infrastructure.Common.Persistence
+{
+    public class HomexDbContext : DbContext, IDbContext
+    {
+        public DbSet<Shelf> Shelves { get; set; } = default!;
+
+        public DbSet<Box> Boxes { get; set; } = default!;
+
+        public DbSet<Item> Items { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Homex.Infrastructure/Shelves/Repositories/ShelfRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Homex.Domain.Shelves.Models;
+using Homex.Domain.Shelves.Repositories;
+using Homex.Infrastructure.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homex.Infrastructure.Shelves.Repositories
+{
+    internal class ShelfRepository : DataRepository<HomexDbContext, Shelf>, IShelfRepository
+    {
+        private readonly IMapper _mapper;
+
+        public ShelfRepository(HomexDbContext db, IMapper mapper) 
+            : base(db)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<Shelf> Find(int id, CancellationToken cancellationToken = default)
+            => await this
+                .All()
+                .FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
+            
+
+    }
+}
